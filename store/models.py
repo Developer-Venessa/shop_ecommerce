a6831f0e0c5543d254d2d4e368a9e7090e65eb81
Developer_Venessa <venessaanyango20@gmail.com>
2024-07-29 11:44:23 +0300

    first commit

---
+++ store/models.py
@@ -0,0 +1,19 @@
+import datetime
+
+from django.db import models
+import datetime
+import os
+
+
+# Create your models here.
+def get_file_path(request, filename):
+    original_filename = filename
+    nowTime = datetime.datetime.now().strftime('%Y%m%d%H:%M:%S')
+    filename = "%s%s" % (nowTime.original_filename)
+    return  os.path.join('uploads/',filename)
+
+
+class Category(models.Model):
+    slug = models.CharField(max_length=150, null=False, blank=False)
+    name = models.CharField(max_length=150, null=False, blank=False)
+    image = models.ImageField(upload_to=get_file_path)
